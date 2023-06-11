ffbc97be29a2ca87031e25fe6c4f30dbaf34a48b
klaudiastarczew <kstarczew@gmail.com>
2023-06-11 11:18:56 +0200

    adding routes

---
+++ routes/hello.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+	var name = req.query.name;
+		res.send('Witaj ' + name + '!');
+	
+});
+
+router.get('/:name', function (req, res, next) {
+	var name = req.params.name;
+	res.send('Witaj ' + name + '!');
+});
+
+module.exports = router;
