448fda41fd6956a053518bbdb4da93fc4443d205
klaudiastarczew <kstarczew@gmail.com>
2023-06-11 11:31:57 +0200

    template engines

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -18,11 +19,22 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+	const start = Date.now();
+	res.on('finish', function () {
+		const end = Date.now();
+		const responseTime = end - start;
+		console.log(`Czas odpowiedzi: ${responseTime}ms`);
+	});
+	next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
